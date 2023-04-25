5df8497c10c91ea3c5c38d830cd650f0b76e3452
Eylem Aytas <eylem.aytas@hotmail.com>
2023-04-25 14:52:49 -0400

    Modifies some files

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const wonderImage = document.getElementById("wonder-image");
 const wonderDescription = document.getElementById("wonder-description");
 const wondersList = document.getElementById("wonders-list");
 const reviewList = document.getElementById("review-list");
+let numOfLikes = 1;
+const likesBox = document.getElementById("likes-box");
+const likesButton = document.getElementById("like-button");
+
 fetch("http://localhost:3000/sevenWonders")
   .then((res) => res.json())
   .then((wonders) => {
     wonders.forEach((wonder) => {
-      appendWonderDetails(wonder);
+      appendWonderDetails(wonders[0]);
       displayWonderNames(wonder);
     });
   });
@@ -15,7 +19,7 @@ fetch("http://localhost:3000/sevenWonders")
 function displayWonderNames(wonder) {
   const listTags = document.createElement("li");
   listTags.textContent = wonder.name;
-  listTags.addEventListener("click", () => {
+  listTags.addEventListener("mouseover", () => {
     appendWonderDetails(wonder);
   });
   wondersList.appendChild(listTags);
@@ -26,6 +30,7 @@ function appendWonderDetails(wonder) {
   wonderImage.src = wonder.image;
   wonderDescription.textContent = wonder.description;
 }
+
 const reviewForm = document.getElementById("review-form");
 reviewForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -34,7 +39,13 @@ reviewForm.addEventListener("submit", (e) => {
 
 function addReview() {
   const review = document.getElementById("review");
-  const reviewLi = document.createElement("li");
+  const reviewLi = document.createElement("p");
   reviewLi.textContent = review.value;
   reviewList.appendChild(reviewLi);
 }
+
+function addLikes() {
+  likesBox.textContent = numOfLikes++ + " likes";
+}
+
+likesButton.addEventListener("click", addLikes);
