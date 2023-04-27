0ca0e3efb44b758037db1f108414c8457957e2d5
Jeremy Kaplan <jeremykaplan10@gmail.com>
2023-04-27 16:48:58 -0400

    modified

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const reviewList = document.getElementById("review-list");
 let numOfLikes = 1;
 const likesBox = document.getElementById("likes-box");
 const likesButton = document.getElementById("like-button");
+const review = document.getElementById("review");
+const audio = new Audio('mixkit-cool-interface-click-tone-2568.wav')
 
 fetch("http://localhost:3000/sevenWonders")
   .then((res) => res.json())
@@ -35,17 +37,39 @@ const reviewForm = document.getElementById("review-form");
 reviewForm.addEventListener("submit", (e) => {
   e.preventDefault();
   addReview();
+  // handleSubmit(e.target.review.value)
+  e.target.reset()
 });
 
 function addReview() {
-  const review = document.getElementById("review");
   const reviewLi = document.createElement("p");
   reviewLi.textContent = review.value;
   reviewList.appendChild(reviewLi);
+
 }
 
 function addLikes() {
   likesBox.textContent = numOfLikes++ + " likes";
+  audio.play()
+  if (numOfLikes === 6) {
+    alert("This wonder has reached 5 likes!")
+  }
 }
 
 likesButton.addEventListener("click", addLikes);
+
+// function handleSubmit(arg1) {
+//   const newComment = {
+//
+//   }
+//   fetch(`http://localhost:3000/sevenWonders/${currentId}`, {
+//     method: "PATCH",
+//     headers: {
+//         "Content-Type": "Application/json"
+//   },
+//   body: JSON.stringify(newComment)
+// })
+// }
+
+
+
